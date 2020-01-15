146d31ce3897d29d0105083340c072136aa1325c
Biggy <jululien.gautier@laposte.net>
2020-01-15 17:40:13 +0100

    Ajoutez des fichiers projet.

---
+++ APIUser/Data/APIUserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIUser.Models
+{
+    public class APIUserContext : DbContext
+    {
+        public APIUserContext (DbContextOptions<APIUserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIUser.Models.User> User { get; set; }
+    }
+}
---
+++ APIUser/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIUser.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
